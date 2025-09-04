a710134716ba5edf95d942007044c904d0f7035e
Atri Goel <atri.goel@gmail.com>
2025-09-04 17:04:10 +0530

    Initial setup

---
+++ ds_and_algorithms/Java/mysecond_file.java
@@ -0,0 +1,8 @@
+public class mysecond_file {
+    public static void main(String[] args) {
+        System.out.println("This is my second file!");
+        for(int i = 10; i > 0; i--) {
+            System.out.println("Countdown: " + i);
+        }
+    }   
+}
